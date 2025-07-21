dd6e2609bebab93a0b2a221c70ed3795154ace65
tshipman44 <tshipman@gmail.com>
2025-07-21 13:16:03 -0700

    updated the tailwind install

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 // No longer need to import dsv
 
 export default defineConfig({
