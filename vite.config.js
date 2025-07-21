59aac34b260a2c97e060e93d53df8546fd70cd7c
tshipman44 <tshipman@gmail.com>
2025-07-21 12:43:18 -0700

    update to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dsv from '@rollup/plugin-dsv' // Use the rollup plugin
+
 
 // https://vite.dev/config/
 export default defineConfig({
